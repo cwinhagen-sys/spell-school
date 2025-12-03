ecba012a6f48de03c8a60b179c0296890a22886f
cwinhagen-sys <c.winhagen@gmail.com>
2025-12-03 19:57:08 +0100

    Update supabase-server.ts

--- src/lib/supabase-server.ts
+++ src/lib/supabase-server.ts
@@ -18,9 +18,9 @@ const supabaseClientOptions = {
   },
   global: {
     // Use fetch with keep-alive for connection reuse
-    fetch: (url: string, options: RequestInit = {}) => {
-      return fetch(url, {
-        ...options,
+    fetch: (input: RequestInfo | URL, init?: RequestInit) => {
+      return fetch(input, {
+        ...init,
         // Enable keep-alive for connection reuse (reduces overhead)
         // Note: keepalive may not be available in all Node.js versions, but is safe to include
         ...(typeof (globalThis as any).Request !== 'undefined' && { keepalive: true }),
